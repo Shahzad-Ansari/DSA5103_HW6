674b48f59618cba429bd1f0ed01021b591ff01fe
Shahzad-Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2021-10-19 17:48:22 -0500

    Add files via upload
    Imputation is mostly done as fast as i know. Used a bit more sophisticated methods.

--- homework_6.R
+++ homework_6.R
@@ -1,4 +1,3 @@
-
 library(mlbench)
 library(dplyr)
 library(ibd)
@@ -31,15 +30,11 @@ library(mice)
 library(FactoMineR)
 library(factoextra)
 library(Hmisc)
-
-
-
+library(fastDummies)
 
 # load and factorize/mutate input data
-training_data <- read.csv("School/DSA5103/Homework 6/Train.csv")
-testing_data <- read.csv("School/DSA5103/Homework 6/Test.csv")
-
 training_data = data.frame(read.csv(file = "School/DSA5103/Homework 6/Train.csv", stringsAsFactors = TRUE)) %>% mutate_all(na_if,"")
+
 # how much of the data is missing?
 percentagesDf = data.frame(colnames(training_data))
 for(i in 1:ncol(training_data)) {       # for-loop over columns
@@ -58,10 +53,9 @@ ggplot(data = percentagesDf, mapping = aes(x = reorder(Column_Name, -Missing_Per
   ylab("Missing Percentage")
 
 sum(is.na(training_data$country))
-
+str(training_data)
 
 # It seems that newVisits to adContent may not be usable.
-
 training_data = dplyr::select(training_data,-c(adwordsClickInfo.isVideoAd,
                                                campaign,
                                                adContent,
@@ -76,122 +70,103 @@ training_data = dplyr::select(training_data,-c(adwordsClickInfo.isVideoAd,
                                                region,
                                                networkDomain,
                                                topLevelDomain,
-                                               bounces
+                                               bounces,
+                                               sessionId
                                                ))
-  
+
+################################################################################################################################
+# IMPUTATION                                                                                                                   #
+################################################################################################################################
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+# imputing the missing values of medium using probability 
+set.seed(1)
+mediumFreq = na.omit(count(training_data,'medium'))
+totalMedium = sum(operatingSystemFreq$freq[1:5])
+mediumFreq[,3] = mediumFreq[,2]/totalMedium
+names(mediumFreq)[3] = "Probability"
+training_data$medium[is.na(training_data$medium)] <-as.factor(sample(mediumFreq$medium[1:5], 
+                                                                size=1, replace=TRUE, prob=mediumFreq$Probability[1:5]
+                                                              ))
+sum(is.na(training_data$medium))
+
+# browser has only 1 na value, just going to use mode imputation on this
+browserFreq = count(training_data,'browser')
+browserFreq
+training_data$browser[is.na(training_data$browser)] <- getmode(training_data$browser) 
+# impute operating System via probability
+operatingSystemFreq = na.omit(count(training_data,'operatingSystem'))
+totaloperatingSystem = sum(operatingSystemFreq$freq[1:15])
+operatingSystemFreq[,3] = operatingSystemFreq[,2]/totaloperatingSystem
+names(operatingSystemFreq)[3] = "Probability"
+training_data$operatingSystem[is.na(training_data$operatingSystem)] <-as.factor(sample(operatingSystemFreq$operatingSystem[1:15], 
+                 size=1, replace=TRUE, prob=operatingSystemFreq$Probability[1:15]
+                 ))
+sum(is.na(training_data$operatingSystem))
+# No way to know if its not or is a new visit as every non NA value is a 1. Cannot compute a probability so i will assume
+# that if it is NA that it is not a new visit. 
+training_data$newVisits[is.na(training_data$newVisits)] = 0
+# imputing page views with rounded down average
+training_data$pageviews[is.na(training_data$pageviews)] = round(mean(!is.na(training_data$pageviews)))
 
 
 # Check if there is a empty country or region or subContinent are all fields empty?
 emptyLocation <- training_data[is.na(training_data$country),] 
 emptyLocation
 
-###################################################
-
-#not working
-
-levels <- levels(training_data$country)
-levels[length(levels) + 1] <- "Unknown"
-
-training_data$country <- factor(training_data$country, levels = levels)
-training_data$country[is.na(training_data$country)] <- "None"
-
-
-
+levels(training_data$country) = c(levels(training_data$country),"Unknown")
+levels(training_data$continent) = c(levels(training_data$continent),"Unknown")
+levels(training_data$subContinent) = c(levels(training_data$subContinent),"Unknown")
 
 training_data$country[is.na(training_data$country)] = as.factor("Unknown")
+training_data$continent[is.na(training_data$continent)] = as.factor("Unknown")
+training_data$subContinent[is.na(training_data$subContinent)] = as.factor("Unknown")
 
-emptyLocation <- training_data[is.na(training_data$country),] 
-emptyLocation
+# Since source has only 2 missing values i will just use mode imputation 
+sourceFreq = count(training_data,'source')
+sourceFreq
+training_data$source[is.na(training_data$source)] <- getmode(training_data$source)
 
-###################################################
-#not working
+# show NA for all columns 
 
-res.famd <- FAMD(training_data, 
-                 graph = TRUE, 
-                 ncp=25)
-
-## Inspect principal components
-print(get_eigenvalue(res.famd))
-###################################################
-
-training_data$browser[is.na(training_data$browser)] <- getmode(training_data$browser) 
-training_data$operatingSystem[is.na(training_data$operatingSystem)] <- getmode(training_data$operatingSystem) 
-training_data$newVisits[is.na(training_data$newVisits)] = 0
 percentagesDf = data.frame(colnames(training_data))
 
 for(i in 1:ncol(training_data)) {       # for-loop over columns
   percentagesDf[i,2] = sum(is.na(training_data[,i]))
-                              
+  
 }
+names(percentagesDf)[1] = "Column name"
+names(percentagesDf)[2] = "Missing Count"
 
 percentagesDf
 
 
-sum(is.na(training_data$isMobile))
-######################################
-#not working
-
-
-md.pattern(training_data)
-
-imputed_data = mice(training_data,m=5,method="rf")
-
-imputed_data
-
-training_data = as.data.frame(training_data)
-
-typeof(training_data)
-
-training_data
-
-my_imp = mice(training_data[,"medium"],method="rf")
-
-miceOutput <- complete(my_imp)
-
-anyNA(miceOutput)
-
-is.na(testing_data$subContinent)
-
-######################################
-# old code
-#Mean value imputation on NA values
-for(i in 1:ncol(training_data)){
-  training_data[is.na(training_data[,i]), i] <- mean(training_data[,i], na.rm = TRUE)
-}
-for(i in 1:ncol(testing_data)){
-  testing_data[is.na(testing_data[,i]), i] <- mean(testing_data[,i], na.rm = TRUE)
-}
-
-head(training_data)
-
-
+################################################################################################################################
+# END OF IMPUTATION                                                                                                            # 
+################################################################################################################################
 
 #Convert all character values and numeric values to ints and factors
-training_data[sapply(training_data, is.character)] <- lapply(training_data[sapply(training_data, is.character)],as.factor)
 training_data[sapply(training_data, is.numeric)] <- lapply(training_data[sapply(training_data, is.numeric)],as.integer)
-
-
-testing_data[sapply(testing_data, is.character)] <- lapply(testing_data[sapply(testing_data, is.character)],as.factor)
-testing_data[sapply(testing_data, is.numeric)] <- lapply(testing_data[sapply(testing_data, is.numeric)],as.integer)
-
-
-# Confrim no N/A Values 
-count = 0
-for(i in 1:ncol(training_data)){
-  count = count + (sum(is.na(training_data[1,i])))
-}
-print(count)
-
-count = 0
-for(i in 1:ncol(testing_data)){
-  count = count + (sum(is.na(testing_data[1,i])))
-}
-print(count)
-
 
 # Confirm the changes have taken place 
 str(training_data)
-training_data$revenue <- log(training_data$revenue)
+################################################################################################################################
+# CREATING DUMMY VARIABLES 
+################################################################################################################################
+test = fastDummies::dummy_cols(training_data,select_columns = c())
+
+test$browser_
+
+
+
+###################################################
+
+
+
+
 
 
 
